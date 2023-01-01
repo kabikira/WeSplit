59bf0802dadb08b03011037ab0a03ac68c718633
kabikira <kabikirar2@protonmail.com>
2023-01-01 19:31:19 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by koala panda on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
